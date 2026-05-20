b9ec13d173b7cb78b991585310babede7dfafdc0
RicardoVPires <ricardovpires@users.noreply.github.com>
2026-05-20 12:30:10 -0300

    feat: campos date_of_birth e country no model Person (US-05)
    - Adicionados campos date_of_birth (DateField) e country (CharField)
    - Migration 0002 aplicada
    - Fixture atualizada com datas de nascimento e country=Brazil para todos

--- contacts/models.py
+++ contacts/models.py
@@ -10,6 +10,8 @@ class Person(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     zip_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=100, default='Brazil')
+    date_of_birth = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
